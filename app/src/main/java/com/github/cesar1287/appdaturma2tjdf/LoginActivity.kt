e69a416651c85e311eeabf1f3f4708755b1f0578
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-04-08 11:42:14 -0300

    Aula 5 - View Binding/Lifecycle

--- app/src/main/java/com/github/cesar1287/appdaturma2tjdf/LoginActivity.kt
+++ app/src/main/java/com/github/cesar1287/appdaturma2tjdf/LoginActivity.kt
@@ -1,12 +1,69 @@
 package com.github.cesar1287.appdaturma2tjdf
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.github.cesar1287.appdaturma2tjdf.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("lifecycle", "onCreate - Login")
+
+        with(binding) {
+            btLoginContinue.setOnClickListener {
+                val name = tieLoginName.text.toString()
+                val email = tieLoginEmail.text.toString()
+                val password = tieLoginPassword.text.toString()
+
+                if (name.isBlank()) {
+                    tilLoginName.error = "Campo de nome vazio e/ou inválido"
+                } else {
+                    tilLoginName.isErrorEnabled = false
+                }
+
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Usuário salvo com sucesso",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("lifecycle", "onStart - Login")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("lifecycle", "onResume - Login")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("lifecycle", "onPause - Login")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("lifecycle", "onStop - Login")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("lifecycle", "onDestroy - Login")
     }
 }
--- app/src/main/java/com/github/cesar1287/appdaturma2tjdf/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/appdaturma2tjdf/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.cesar1287.appdaturma2tjdf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("lifecycle", "onCreate - Main")
 
         val helloWorld = findViewById<TextView>(R.id.tvMainHelloWorld)
         helloWorld.text = getString(R.string.hello_world)
@@ -18,5 +20,35 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btMainTapMe).setOnClickListener {
             helloWorld.text = "O número sorteado é ${(Math.random()*100).toInt()}"
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("lifecycle", "onStart - Main")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("lifecycle", "onResume - Main")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("lifecycle", "onPause - Main")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("lifecycle", "onStop - Main")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("lifecycle", "onDestroy - Main")
     }
 }
